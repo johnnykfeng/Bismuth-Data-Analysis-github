09d21d32cee972388ecc2d4c9c0ca09f14c8289c
John K Feng <38768263+johnnykfeng@users.noreply.github.com>
2021-09-14 19:10:56 -0400

    Good progress on querying rad_avg big data

--- Query Plot Rad_Avg.py
+++ Query Plot Rad_Avg.py
@@ -14,5 +14,43 @@ from sqlalchemy import create_engine, MetaData, Table, Column, String, Integer, 
     ARRAY, Numeric, Float, ForeignKey, ForeignKeyConstraint, column
 from sqlalchemy import insert, update, select, inspect
 
-csv_direc = 'D:\\Bismuth Project\\Bismuth-Data-Analysis-github\\peak_sum_sql_export.csv'
-peak_sum_df = pd.read_csv(csv_direc, index_col=[0])
+# csv_direc = 'D:\\Bismuth Project\\Bismuth-Data-Analysis-github\\Big_Scan_14nm.csv'
+# radavg_flat_df = pd.read_csv(csv_direc, index_col=[1], usecols = ['timepoint', 'scan_key', 'radavg_flattened'])
+#
+# # print(radavg_flat[:5])
+#
+# shortdata = radavg_flat_df[:1]
+# radavg_flattened = shortdata.iloc[0]['radavg_flattened']
+# # radavg_flattened = radavg_flattened.array
+#
+# # shortdata.plot(x = 'timepoint', y = 'radavg_flattened', kind = 'line')
+# plt.plot(np.array(radavg_flattened))
+# plt.show()
+
+using_sql = 1
+if using_sql:
+    # sqlalchemy connection to postgresql database
+    engine = create_engine('postgresql://postgres:sodapop1@localhost:7981/Bismuth_Project')
+    metadata = MetaData()
+    connection = engine.connect()
+
+    scandict = Table('scandictionary', metadata, autoload=True, autoload_with=engine, extend_existing=True)
+    bigscan14nm = Table('Big_Scan_14nm', metadata, autoload=True, autoload_with=engine, extend_existing=True)
+
+    peak_scan_list = []
+    timepoint_list = []
+
+    col = [bigscan14nm.c.radavg_flattened]
+
+    stmt = select(col)
+    stmt = stmt.where(bigscan14nm.c.scan_key == 11)
+    result = connection.execute(stmt).fetchall()
+
+plt.figure()
+
+for i in np.arange(0,20,5):
+    y = result[i][0]
+    x = range(len(y))
+
+    plt.plot(y)
+plt.show()
