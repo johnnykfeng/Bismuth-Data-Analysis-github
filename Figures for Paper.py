75c03dd66626b91258ad7db826b5521ce860382d
John K Feng <38768263+johnnykfeng@users.noreply.github.com>
2021-10-28 15:16:06 -0400

    Colorbar radavg figure

--- Figures for Paper.py
+++ Figures for Paper.py
@@ -76,8 +76,6 @@ def figure1(timepoint_on = 15.0, timepoint_off =-3.0, scankey = 2, savefigure = 
     plt.ylim(1.2*min(radavgdiff), 9000)
     ax.legend(loc='upper right', prop={'size': 10}, frameon=False)
     plt.tight_layout()
-
-    plt.tight_layout()
     # plt.grid(True)
     plt.show()
 
@@ -88,7 +86,7 @@ def figure1(timepoint_on = 15.0, timepoint_off =-3.0, scankey = 2, savefigure = 
         plt.savefig(saveDirectory + figureName +'.svg', format='svg', dpi = 600)
         plt.savefig(saveDirectory + figureName +'.png', format='png', dpi = 600)
 
-figure1(timepoint_on = 15.0, timepoint_off =-3.0, scankey=2, savefigure = 0)
+# figure1(timepoint_on = 15.0, timepoint_off =-3.0, scankey=2, savefigure = 0)
 
 #------------------------------------------------------------------------------
 
@@ -115,7 +113,7 @@ def figure2(scankey = 2, savefigure = 0):
     tp = peak_df['timepoint']
 
     # peak_choice = 0, 1, 2, 3, 4, 5
-    peak_choice = 0, 1, 4, 5
+    peak_choice = 0, 1, 4, 5, 6
     peak_colors = cm.turbo(np.linspace(0, 1, len(peak_choice)))
 
     fig, ax = plt.subplots()
@@ -166,62 +164,174 @@ def figure2(scankey = 2, savefigure = 0):
 
     plt.show()
 
-# figure2(scankey = 2, savefigure = 0)
+figure2(scankey = 2, savefigure = 0)
 
-# def figure3():
+def figure_DebyeWaller(linearplot = True):
+    import Linear_fit
+    peaksum_23nm_df = pd.read_sql_table('Peak_sum_23nm_adjusted',
+                                        'postgresql://postgres:sodapop1@localhost:7981/Bismuth_Project')
+    peak_cols = ['peak1', 'peak2', 'peak3', 'peak4', 'peak5', 'peak6', 'peak7',
+                 'peak8', 'peak9', 'peak10', 'peak11', 'peak12', 'peak13', 'peak14', 'peak15']
+    fluence_label = ['0.78','1.3', '2.6', '5.2', '7.8', '10.4', '15.6']
+    fig, ax = plt.subplots()
 
+    if linearplot:
+        scankey_loop = np.arange(1,4)
+    else:
+        scankey_loop = np.arange(1,6)
 
-import Linear_fit
-peaksum_23nm_df = pd.read_sql_table('Peak_sum_23nm_adjusted',
-                                    'postgresql://postgres:sodapop1@localhost:7981/Bismuth_Project')
-scankey = 2
-peak_cols = ['peak1', 'peak2', 'peak3', 'peak4', 'peak5', 'peak6', 'peak7',
-             'peak8', 'peak9', 'peak10', 'peak11', 'peak12', 'peak13', 'peak14', 'peak15']
-fluence_label = ['0.78','1.3', '2.6', '5.2', '7.8', '10.4', '15.6']
-fig,ax = plt.subplots()
+    print(scankey_loop)
+    colors_fluence = cm.Set1(np.linspace(0, 1, len(fluence_label)))
+    for scanindex, scankey in enumerate(scankey_loop):
+        peak_df = peaksum_23nm_df[peaksum_23nm_df['scan_id'] == scankey]
+        peak_choice = 0, 1, 4, 5
+        DBy_list = []
+        DBx_list = []
+        peakposns = np.array([125, 186, 218, 263, 302, 333, 388, 430, 451, 479]) # peakposns are accurate to +/- 1 pixel
+        peakposn_k = peakposns*pixel2Ghkl
 
-scankey_loop = np.arange(1,4)
-print(scankey_loop)
-
-colors_fluence = cm.Set1(np.linspace(0, 1, len(fluence_label)))
-
-for scanindex, scankey in enumerate(scankey_loop):
-
-    peak_df = peaksum_23nm_df[peaksum_23nm_df['scan_id'] == scankey]
-
-    peak_choice = 0, 1, 4, 5
-    peak_colors = cm.turbo(np.linspace(0, 1, len(peak_choice)))
-    DBy_list = []
-    DBx_list = []
-    peakposns =np.array([125, 186, 218, 263, 302, 333, 388, 430, 451, 479])
-    peakposn_k = peakposns*pixel2Ghkl
-
-
-    for p, peak_index in enumerate(peak_choice):  # loop over peaks
-        peak = peak_df.loc[:, peak_cols[peak_index]]
-        peak_norm = peak / np.mean(peak[:6])
-        DBy = -np.log(np.mean(peak_norm[-5:]))   # average the last 5 peak_norm
-        DBx = (peakposn_k[peak_index])
-        DBx_list.append(DBx)
-        DBy_list.append(DBy)
-
-    dbx_fit, dby_fit, popt, pcov = Linear_fit.mainfit(x=DBx_list, y=DBy_list,
-                                                      slope_guess=.4, intercept_guess=0, include_intercept=True)
-    axlabel = fluence_label[scanindex] + ' mJ/cm$^2$'
-    print(axlabel)
-    ax.plot(DBx_list, DBy_list, marker = 'o', linewidth=1, label= axlabel, color = colors_fluence[scanindex])
-    lowerbound = 26
-    ax.plot(dbx_fit[lowerbound:], dby_fit[lowerbound:], linestyle = '--', color = colors_fluence[scanindex])
-
-    ax.set_ylabel('$-ln( I(t)/I_{t0} )$')
-    ax.set_xlabel('$G_{hkl}^2$  ' + ' ($A^{-2}$) ')
-    ax.set_xlim(0.25, 0.75)
-
-plt.grid(True)
-plt.legend()
-plt.show()
+        for p, peak_index in enumerate(peak_choice):  # loop over peaks
+            peak = peak_df.loc[:, peak_cols[peak_index]]
+            peak_norm = peak / np.mean(peak[:6])
+            DBy = -np.log(np.mean(peak_norm[-5:]))   # average the last 5 peak_norm
+            DBx = (peakposn_k[peak_index])
+            DBx_list.append(DBx)
+            DBy_list.append(DBy)
 
 
+        axlabel = fluence_label[scanindex] + ' mJ/cm$^2$'
+        print(axlabel)
+        ax.plot(DBx_list, DBy_list, marker = 'o',markersize = 6, linewidth=1, label= axlabel, color = colors_fluence[scanindex])
 
-# dby.append(-np.log(sum(y_peaks[-5:]) / 5.0))  # ln of average of last 5 data points in the trace
-# dbx.append((peakposition[p] * pixel2Ghkl) ** 2)  # square of the scattering vector
+        if linearplot:
+            dbx_fit, dby_fit, popt, pcov = Linear_fit.mainfit(x=DBx_list, y=DBy_list,
+                                                              slope_guess=.4, intercept_guess=0, include_intercept=True)
+            lowerbound = 26
+            ax.plot(dbx_fit[lowerbound:], dby_fit[lowerbound:], linestyle = '--', color = colors_fluence[scanindex])
+            ax.set_xlim(0.25, 0.75)
+            ax.set_ylim(-0.05, 0.40)
+        else:
+            ax.set_xlim(0.25, 0.75)
+            ax.set_ylim(-0.05, 1.8)
+
+        ax.set_ylabel('-ln($I(t)/I_{t0}$)', labelpad=5, fontsize=12)
+        ax.set_xlabel('k$^2$' + ' ($A^{-2}$) ', fontsize=12)
+        ax.tick_params(axis='both', labelsize=12)
+        ax.legend(loc='upper left', prop={'size': 10}, frameon=False)
+
+    plt.tight_layout()
+    # plt.grid(True)
+    plt.show()
+
+# figure_DebyeWaller(linearplot=False)
+# figure_DebyeWaller(linearplot=True)
+
+
+def figure_colorbar_flatradavg():
+    bigscan23nm_df = pd.read_sql_table('Big_Scan_23nm', 'postgresql://postgres:sodapop1@localhost:7981/Bismuth_Project')
+    scan_loop_arange = np.arange(1,8,1) # for 23nm scan
+    # scan_loop_arange = np.arange(1,3,1) # for 23nm scan
+    print(len(scan_loop_arange))
+    liquid_rise_list = [None] * len(scan_loop_arange)
+    peak_choices = 0, 1, 4, 5
+
+
+    for scanindex, scankey in enumerate(scan_loop_arange):  # loop over scans
+        fluence = scandict_df[scandict_df['scan_id'] == scankey]['fluence'].values
+
+        # fig, ax = plt.subplots()
+        fig_str_label = 'scankey: ' + str(scankey) + ', fluence: ' +str(fluence)
+        plt.figure(fig_str_label)
+
+        # plt.title(fig_str_label)
+        # scandf = bigscan14nm_df[bigscan14nm_df['scan_key'].isin([scankey])] # filters for scankey, partition the df
+        scandf = bigscan23nm_df[bigscan23nm_df['scan_key'].isin([scankey])] # filters for scankey, partition the df
+        tp = np.array(scandf['timepoint'])     # extract the 'timepoint' column
+        radavgflat = scandf['radavg_flattened']    # extract the 'radavg_flattened' column
+
+        colors = cm.jet(np.linspace(0, 1, len(tp)))    # colormap for plotting
+
+        sm = plt.cm.ScalarMappable(cmap='jet',
+                                   norm=plt.Normalize(vmin=np.min(tp), vmax=np.max(tp)))
+
+        for j, radavgflat_plot in enumerate(radavgflat): # this loops over every timepoint
+
+            radavgflat_plot = np.array(radavgflat_plot)  # radavgflat_plot is an array for one timepoint
+            plt.plot(radavgflat_plot, color=colors[j], label=str(tp[j]))
+
+            if j == 0:  # for the first timepoint, get peakposn and bases
+                peakposn, properties = find_peaks(radavgflat_plot, threshold=None, distance=20, prominence=2, width=2)
+                bases = argrelmin(radavgflat_plot, order=20)[0]
+                # plt.plot(peakposn, radavgflat_plot[peakposn], 'x')  # mark the peaks with x
+                # plt.plot(bases, radavgflat_plot[bases], 'o')  # mark the bases with o
+
+        cbar = plt.colorbar(sm)
+        cbar.set_label('Time Delay (ps)')
+
+        plt.xlim(90,370)
+        plt.ylim(-300,8500)
+        # plt.yticks([])
+        plt.ylabel('Intensity (a.u.)', labelpad=5, fontsize=12)
+        plt.xlabel(r'k ($\AA^{-1}$)', fontsize=12)
+        # plt.legend()
+        # plt.grid(True)
+
+    plt.show()
+
+# figure_colorbar_flatradavg()
+
+def figure_colorbar_off():
+    bigscan23nm_df = pd.read_sql_table('Big_Scan_23nm', 'postgresql://postgres:sodapop1@localhost:7981/Bismuth_Project')
+    scan_loop_arange = np.arange(7,8,1) # for 23nm scan
+    # scan_loop_arange = np.arange(1,3,1) # for 23nm scan
+    print(len(scan_loop_arange))
+    liquid_rise_list = [None] * len(scan_loop_arange)
+    peak_choices = 0, 1, 4, 5
+
+
+    for scanindex, scankey in enumerate(scan_loop_arange):  # loop over scans
+        fluence = scandict_df[scandict_df['scan_id'] == scankey]['fluence'].values
+
+        # fig, ax = plt.subplots()
+        fig_str_label = 'scankey: ' + str(scankey) + ', fluence: ' +str(fluence)
+        plt.figure(fig_str_label)
+
+        # plt.title(fig_str_label)
+        # scandf = bigscan14nm_df[bigscan14nm_df['scan_key'].isin([scankey])] # filters for scankey, partition the df
+        scandf = bigscan23nm_df[bigscan23nm_df['scan_key'].isin([scankey])] # filters for scankey, partition the df
+        tp = np.array(scandf['timepoint'])     # extract the 'timepoint' column
+        radavgflat = scandf['radavg_off']    # extract the 'radavg_flattened' column
+
+        colors = cm.jet(np.linspace(0, 1, len(tp)))    # colormap for plotting
+
+        sm = plt.cm.ScalarMappable(cmap='jet',
+                                   norm=plt.Normalize(vmin=np.min(tp), vmax=np.max(tp)))
+
+        for j, radavgflat_plot in enumerate(radavgflat): # this loops over every timepoint
+
+            radavgflat_plot = np.array(radavgflat_plot)  # radavgflat_plot is an array for one timepoint
+            px = np.arange(0,len(radavgflat_plot))
+            x_plot = px*pixel2Ghkl
+            plt.plot(x_plot, radavgflat_plot, color=colors[j], label=str(tp[j]))
+
+            if j == 0:  # for the first timepoint, get peakposn and bases
+                peakposn, properties = find_peaks(radavgflat_plot, threshold=None, distance=20, prominence=2, width=2)
+                bases = argrelmin(radavgflat_plot, order=20)[0]
+                # plt.plot(peakposn, radavgflat_plot[peakposn], 'x')  # mark the peaks with x
+                # plt.plot(bases, radavgflat_plot[bases], 'o')  # mark the bases with o
+
+        cbar = plt.colorbar(sm)
+        cbar.set_label('Time Delay (ps)')
+
+        plt.xlim(0.23, 0.9)
+        # plt.ylim(-300,8500)
+        plt.ylim(500,9500)
+        plt.yticks([])  # empty y-ticks for arbituary units
+        plt.ylabel('Intensity (a.u.)', labelpad=5, fontsize=12)
+        plt.xlabel(r'k ($\AA^{-1}$)', fontsize=12)
+        # plt.legend()
+        # plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+
+figure_colorbar_off()
